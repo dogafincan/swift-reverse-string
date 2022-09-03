a8e261660e11a214c191a86a3479784548ff3065
Doga Fincan <doga@icloud.com>
2022-09-03 20:32:16 +0200

    Initial Commit

---
+++ Tests/ReverseStringTests/ReverseStringTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import ReverseString
+
+final class ReverseStringTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(ReverseString().text, "Hello, World!")
+    }
+}
